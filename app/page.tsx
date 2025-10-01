e4c98d027fae5e7fa9f36f07af042d5152f1e635
Jacob <113521203+sabian4695@users.noreply.github.com>
2025-10-01 15:51:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,65 +14,70 @@ import Avatar from '@mui/material/Avatar';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import InsertLinkIcon from '@mui/icons-material/InsertLink';
+import Grow from '@mui/material/Grow';
 
 export default function App() {
   return (
     <>
       <Stack sx={{ width: '100%' }}>
-        <Box sx={{ p: 10 }}>
-          <Image
-            height='250'
-            width='250'
-            src='/images/logo.png'
-            alt='logo'
-            loading="lazy"
-          />
-          <Typography
-            sx={{ fontWeight: '100', letterSpacing: 7 }}
-            variant={'h3'}
-            color='text.secondary'
-          >
-            WorkingDB
-          </Typography>
-        </Box>
+        <Grow in={true}>
+          <Box sx={{ p: 10 }}>
+            <Image
+              height='250'
+              width='250'
+              src='/images/logo.png'
+              alt='logo'
+              loading="lazy"
+            />
+            <Typography
+              sx={{ fontWeight: '100', letterSpacing: 7 }}
+              variant={'h3'}
+              color='text.secondary'
+            >
+              WorkingDB
+            </Typography>
+          </Box>
+        </Grow>
         <Box>
           <Divider variant="middle" flexItem sx={{ m: 5 }} />
           <Typography variant="h3">
             Welcome to the WorkingDB Help Site
           </Typography>
-          <Paper elevation={3} sx={{ borderRadius: '10px', p: 3, m: 5 }}>
-            <Stack sx={{ alignItems: 'center', justifyContent: 'center' }} spacing={3}>
-              <Typography variant="h5" color='text.secondary'>There are three main ways to navigate</Typography>
-              <Box>
-                <List sx={{ bgcolor: 'background.paper' }}>
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <InsertLinkIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Click a link from WorkingDB or elsewhere" secondary="Every page and header has a direct link" />
-                  </ListItem>
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <AccountTreeIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="The Tree" secondary="Use the tree on the left to navigate to a specific page or just browse" />
-                  </ListItem>
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>
-                        <SearchIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Search" secondary="Just start typing in the search bar on the top right and go!" />
-                  </ListItem>
-                </List>
-              </Box>
-            </Stack>
-          </Paper>
+          <Grow in={true}>
+            <Paper elevation={3} sx={{ borderRadius: '10px', p: 3, m: 5 }}>
+              <Stack sx={{ alignItems: 'center', justifyContent: 'center' }} spacing={3}>
+                <Typography variant="h5" color='text.secondary'>There are three main ways to navigate</Typography>
+                <Box>
+                  <List sx={{ bgcolor: 'background.paper' }}>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <InsertLinkIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Click a link from WorkingDB or elsewhere" secondary="Every page and header has a direct link" />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <AccountTreeIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="The Tree" secondary="Use the tree on the left to navigate to a specific page or just browse" />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <SearchIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary="Search" secondary="Just start typing in the search bar on the top right and go!" />
+                    </ListItem>
+                  </List>
+                </Box>
+              </Stack>
+            </Paper>
+          </Grow>
         </Box>
       </Stack>
     </>
